de65dbef0c19cf6dedb1d108b7b39ff9f742551b
mariusgessler <marius.gessler@gmail.com>
2020-05-30 13:40:48 +0200

    Style button

--- src/components/nanos/RemovePlayer.js
+++ src/components/nanos/RemovePlayer.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
 import { connect } from 'pwa-helpers'; // connects a Custom Element base class to the Redux store
 import { store } from '../../redux/store.js';
 import { removePlayer } from '../../redux/actions.js';
@@ -13,20 +13,27 @@ export class RemovePlayer extends connect(store)(LitElement) {
     };
   };
 
+  static get styles() {
+    return css`
+     mwc-button {
+      --mdc-theme-primary: #B00020;
+     }`
+  }
+
   stateChanged(state) {
     this.players = state.players;
-  }
+  };
 
   removePlayer() {
     if (this.players) {
       store.dispatch(removePlayer())
-    }
-  }
+    };
+  };
 
   render() {
     return html`
-    <mwc-button raised ?disabled = ${!Object.values(this.players).length}  icon="remove" @click="${this.removePlayer}">Remove Player</mwc-button>`
-  }
-}
+    <mwc-button class='themed' raised ?disabled = ${!Object.values(this.players).length}  icon="remove" @click="${this.removePlayer}">Remove Player</mwc-button>`
+  };
+};
 
 customElements.define('remove-player', RemovePlayer);
