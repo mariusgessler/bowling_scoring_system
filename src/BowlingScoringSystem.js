f7d0c04268ba2c1b29aaa220c34e64345197265e
mariusgessler <marius.gessler@gmail.com>
2020-06-01 22:51:29 +0200

    Add TotalWinner component

--- src/BowlingScoringSystem.js
+++ src/BowlingScoringSystem.js
@@ -1,13 +1,15 @@
 import { LitElement, html } from 'lit-element';
 import './components/PlayerScoreboard.js';
-import './components/ScoreboardControls.js';
 import './components/GameControls.js';
+import './components/nanos/TotalWinners.js';
 
 export class BowlingScoringSystem extends LitElement {
+
   render() {
     return html`
     <player-scoreboard></player-scoreboard>
-    <scoreboard-controls></scoreboard-controls>
-    <game-controls></game-controls>`
-  }
+    <game-controls></game-controls>
+    <total-winners></total-winners>
+    `
+  };
 }
---
+++ src/components/nanos/TotalWinners.js
@@ -0,0 +1,43 @@
+import { LitElement, html, css } from 'lit-element';
+import { connect } from 'pwa-helpers';
+import { store } from '../../redux/store.js';
+import '@material/mwc-list';
+
+export class TotalWinners extends connect(store)(LitElement) {
+  static get properties() {
+    return {
+      players: {
+        type: Array
+      }
+    }
+  }
+
+  static get styles() {
+    return css`
+    mwc-list {
+      margin: 65px auto;
+      width: 250px;
+      min-height: 300px;
+    }`
+  }
+
+  stateChanged(state) {
+    this.players = state.players;
+  }
+
+  render() {
+    return html`
+    <mwc-list>
+      <mwc-list-item>
+        <span>Total Wins</span>
+      </mwc-list-item>
+      <li divider role="separator"></li>
+    ${this.players.map((player) => html`
+      <mwc-list-item>${player.name}: ${player.totalWins}</mwc-list-item> <br/>
+    ` )}
+    </mwc-list>
+    `
+  }
+}
+
+customElements.define('total-winners', TotalWinners);
