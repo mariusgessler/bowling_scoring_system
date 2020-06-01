404cf367b38388d53e389d524572ed8a227a10c9
mariusgessler <marius.gessler@gmail.com>
2020-06-01 22:51:04 +0200

    Add getWinner funcionality

--- src/logic/game.js
+++ src/logic/game.js
@@ -1,5 +1,5 @@
 import { store } from '../redux/store.js';
-import { addRoll, nextFrame, nextPlayer } from '../redux/actions.js';
+import { addRoll, nextFrame, nextPlayer, setWinner } from '../redux/actions.js';
 
 export const getGameTotal = (rolls) => {
   let total = 0;
@@ -11,7 +11,7 @@ export const getGameTotal = (rolls) => {
       });
     };
   });
-  return total; // Shave off the strike symbol
+  return total;
 };
 
 export const getFrameTotal = (rolls) => {
@@ -20,34 +20,57 @@ export const getFrameTotal = (rolls) => {
     total += roll;
   })
   return total;
-}
+};
+
+export const getWinner = () => {
+  const totalScores = {};
+  const { players } = store.getState();
+
+  players.map((player) => {
+    totalScores[player.id] = getGameTotal(player.rolls);
+  })
+  const winner = Object.keys(totalScores).reduce((a, b) => totalScores[a] > totalScores[b] ? a : b);
+  store.dispatch(setWinner(winner));
+};
+
+export const isGameOver = (players) => {
+  const lastPlayer = players.length - 1;
+  let lastFramePlayed = false;
+  lastFramePlayed = players[lastPlayer].currentFrame === 10 && players[lastPlayer].rolls[10].length === 2;
+  if (lastFramePlayed) {
+    getWinner();
+  };
+  return lastFramePlayed;
+};
 
 export const calculateRolls = (frame,roll, player) => {
   store.dispatch(addRoll(frame, roll, player));
 
   const getPrevRoll = ( rollsBack ) => {
     return store.getState().players.find((i) => i.id === player).rolls[frame - rollsBack]
-  }
-  const currentRoll = store.getState().players.find((i) => i.id === player).rolls[frame]
+  };
 
+  const currentRoll = store.getState().players.find((i) => i.id === player).rolls[frame];
+
+  // Handle strikes
   if (currentRoll[0] === 10) {
     store.dispatch(addRoll(frame, 0, player));
-  } 
+  }
 
-    if (getPrevRoll(1) && getPrevRoll(1)[0] === 10) {
-      store.dispatch(addRoll(frame - 1,roll, player ))
-      if (getPrevRoll(2) && getPrevRoll(2)[0] === 10) {
-        store.dispatch(addRoll(frame - 2,roll, player ))
-      }
+  if (getPrevRoll(1) && getPrevRoll(1)[0] === 10) {
+    store.dispatch(addRoll(frame - 1,roll, player ))
+    if (getPrevRoll(2) && getPrevRoll(2)[0] === 10) {
+      store.dispatch(addRoll(frame - 2,roll, player ))
     }
+  }
 
+  // Handle spares
   if (getPrevRoll(1) && (getPrevRoll(1)[0] + getPrevRoll(1)[1] === 10) && (getPrevRoll(1)[0] !== 10)) {
     if(currentRoll.length !== 2) {
       store.dispatch(addRoll(frame - 1,currentRoll[0], player ))
     }
   }
-}
-
+};
 
 export const advanceGame = (players, roll, currentPlayer) => {
   const lastPlayer = players.length - 1 === currentPlayer;
@@ -73,8 +96,3 @@ export const advanceGame = (players, roll, currentPlayer) => {
     calculateRolls(currentFrame, roll, players[currentPlayer].id);
   }
 };
-
-export const getRemaining = (players, roll, currentPlayer) => {
-  const  { currentFrame } = players[currentPlayer];
-  return 10 - players[currentPlayer].rolls[currentFrame];
-}
